4f325a9cf16b6a0cf6cbd0f4396e146bb31712db
Brandon <bmacdonald1986@gmail.com>
2021-05-27 04:30:57 +0000

    Add product and category info to admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "jungle", password: "book"
+  #http_basic_authenticate_with name: "jungle", password: "book"
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
