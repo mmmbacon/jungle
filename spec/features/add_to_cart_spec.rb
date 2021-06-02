ea26f0f3b0d6f7a7d7fab29cfca2702b5eadd20d
Brandon <bmacdonald1986@gmail.com>
2021-06-02 17:01:58 +0000

    Add visual tests for adding item to cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
     10.times do |n|
       @category.products.create!(
-        name:  Faker::Hipster.sentence(n),
-        description: Faker::Hipster.paragraph(n),
+        name:  Faker::Hipster.sentence(1),
+        description: Faker::Hipster.paragraph(1),
         image: open_asset('apparel1.jpg'),
         quantity: 10*n,
         price: 64.99
@@ -16,14 +16,16 @@ RSpec.feature "ProductDetails", type: :feature do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see the product cart" do
     visit root_path
 
-    click_on @category.products[0].name
+    puts page.html
+    find(".product", match: :first).click_on("Add")
     # commented out b/c it's for debugging only
-    #save_and_open_screenshot
+    save_and_open_screenshot
 
-    expect(page).to have_content(@category.products[0].name)
+    expect(page).to have_content("My Cart (1)")
   end
+
 
 end
